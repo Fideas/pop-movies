bf89b641055259db3bf812ebd44a6196f7aaf8b5
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2015-09-21 17:42:48 -0300

    Add the trailer Thumbnail to the UI

--- app/src/main/java/com/nicolascarrasco/www/popular_movies/TrailerListAdapter.java
+++ app/src/main/java/com/nicolascarrasco/www/popular_movies/TrailerListAdapter.java
@@ -1,36 +1,56 @@
 package com.nicolascarrasco.www.popular_movies;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 /**
  * Created by Nicolás Carrasco on 21-09-2015.
  */
-public class TrailerListAdapter extends ArrayAdapter{
+public class TrailerListAdapter extends ArrayAdapter {
 
     private LayoutInflater mInflater;
     private ArrayList<Trailer> mTrailerList;
+    private Context mContext;
+    private final static String LOG_TAG = TrailerListAdapter.class.getSimpleName();
 
-    public TrailerListAdapter(Context context, ArrayList<Trailer> data){
-        super(context,R.layout.list_item_trailer, data);
+    public TrailerListAdapter(Context context, ArrayList<Trailer> data) {
+        super(context, R.layout.list_item_trailer, data);
         mInflater = LayoutInflater.from(context);
         mTrailerList = data;
+        mContext = context;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null){
+        if (convertView == null) {
             convertView = mInflater.inflate(R.layout.list_item_trailer, parent, false);
         }
-        TextView trailerTitleTextView = (TextView)convertView
+        TextView trailerTitleTextView = (TextView) convertView
                 .findViewById(R.id.trailer_name_text_view);
-        Trailer trailer = mTrailerList.get(position);
+        ImageView trailerThumbnail = (ImageView) convertView.findViewById(R.id.trailer_thumbnail);
+        Trailer trailer = (Trailer) this.getItem(position);
+        String thumbnailPath = "http://img.youtube.com/vi/" + trailer.getTrailerKey()+ "/0.jpg";
+        try {
+            Picasso
+                    .with(mContext)
+                    .load(thumbnailPath)
+                    .resize(250, 125)
+                    .centerCrop()
+                    .into(trailerThumbnail);
+
+        } catch (IllegalArgumentException e){
+            Log.e(LOG_TAG, "Malformed/Missing URL", e);
+        }
         trailerTitleTextView.setText(trailer.getTrailerName());
         return convertView;
     }
